642b0d5780c35d6d43e47576afa608ab362b003f
Julien <julamb@me.com>
2021-07-30 19:12:02 +0200

    add bubbles animation

--- js/bubbles.js
+++ js/bubbles.js
@@ -1,5 +1,26 @@
 (function() {
+
   const svg = d3.select('.animation.bubbles svg');
-  makeGradientIdsUnique(svg);
-  pulse(svg.selectAll('path'), 0.7)
+  const paths = svg.selectAll('path');
+
+  function init() {
+    makeGradientIdsUnique(svg);
+    paths.attr('transform', (d,i,nodes) => scaleCenter(nodes[i], 0));
+  }
+
+  function appear() {
+    return paths
+      .transition()
+      .duration(300)
+      .delay((d,i) => 150*(2-i))
+      .attr('transform', (d,i,nodes) => scaleCenter(nodes[i], 1))
+      .end();
+  }
+
+  function perpetuate() {
+    pulse(svg.selectAll('path'), 0.7)
+  }
+
+  init();
+  runOnScrollIn(svg.node(), () => appear().then(perpetuate));
 })();
